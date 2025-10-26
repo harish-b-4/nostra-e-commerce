8a3217026c58ddce707fd7eb2bcc046f9d54a186
harish-b-4 <harishb.contact@gmail.com>
2025-10-26 09:54:27 +0530

    Updated SearchBar

--- collections.js
+++ collections.js
@@ -35,64 +35,106 @@ sidenavclose.addEventListener("click", function () {
 
 
 
+// SearchBar Functionality
 
-//CheckBox Selection
+
+
+var productsContainer = document.querySelector(".products")
+
+
+var productsList = productsContainer.querySelectorAll("div")
+
+
+var search = document.getElementById("search-bar")
+
+
+search.addEventListener("keyup", function (event) {
+
+
+    var enteredValue = event.target.value.toUpperCase()
+
+
+    productsList.forEach(function (product) {
+
+        var productName = product.querySelector("h2").textContent.toUpperCase()
+
+
+        if (productName.indexOf(enteredValue) < 0) {
+            product.style.display = "none"
+        } else {
+
+            product.style.display = "block"
+        }
+    })
+})
+
+
+
+
+
+
+
+
+// CheckBox Functionality
+
 
 
 var checkboxes = document.querySelectorAll('input[type="checkbox"][name="tags"]')
 
-var products = document.querySelectorAll('.products > div')
+
+var allProducts = document.querySelectorAll('.products > div')
 
 
 var originalDisplay = []
-products.forEach(function(product) {
+allProducts.forEach(function (product) {
     originalDisplay.push(product.style.display || 'block')
 })
 
 
 var filterMap = {
 
-    summer: [0, 8],        
-    party: [2, 5, 6, 7],    
-    Beach: [1, 3, 4],       
+    summer: [0, 8],
+    party: [2, 5, 6, 7],
+    Beach: [1, 3, 4],
 
-   
-    red: [2, 5, 6, 7],      
-    Blue: [0, 8],           
-    white: [3],             
-    green: [1],             
 
-    
-    new: [0, 1, 8],         
-    old: [2, 3, 4, 5]       
+    red: [2, 5, 6, 7],
+    Blue: [0, 8],
+    white: [3],
+    green: [1],
+
+
+    new: [0, 1, 8],
+    old: [2, 3, 4, 5]
 }
 
 
-checkboxes.forEach(function(checkbox) {
+checkboxes.forEach(function (checkbox) {
+    checkbox.addEventListener('change', function () {
 
-    checkbox.addEventListener('change', function() {
-     
+
         var checkedValues = Array.from(checkboxes)
+            .filter(function (cb) { return cb.checked })
+            .map(function (cb) { return cb.value })
 
-            .filter(function(cb) { return cb.checked })
-            .map(function(cb) { return cb.value })
 
         if (checkedValues.length === 0) {
-           
-            products.forEach(function(product, index) {
+            allProducts.forEach(function (product, index) {
                 product.style.display = originalDisplay[index]
             })
-        } 
+        }
+
         else {
-            
-            products.forEach(function(product, index) {
+            allProducts.forEach(function (product, index) {
                 var shouldShow = false
 
-                checkedValues.forEach(function(value) {
+
+                checkedValues.forEach(function (value) {
                     if (filterMap[value] && filterMap[value].includes(index)) {
                         shouldShow = true
                     }
                 })
+
 
                 product.style.display = shouldShow ? originalDisplay[index] : 'none'
             })
